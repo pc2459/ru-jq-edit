a1a228f2fc59840ba7114e54d5f47cfc58c4b274
Po Linn Chia <chiapolinn@gmail.com>
2015-01-20 09:34:43 -0700

    First working commit

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
 $(document).on('ready', function() {
-  
+
+	
+
+	$(".editable").on("click",function(){
+		var input = $("<input type='text'>");
+		var clicked = $(this);
+		// hide the textblock
+		clicked.hide();
+
+
+		// Set input as same width as text block 
+		input.width(clicked.width());
+		input.height(clicked.height());
+		input.val(clicked.text());
+
+		// Insert after text block
+		clicked.after(input);
+
+		// Focus
+		input.focus();
+
+		input.blur(function(){
+			console.log("Blurred");
+
+			clicked.text(input.val());
+			input.hide();
+			
+			console.log($(this).parent());
+			
+			clicked.show();
+		})
+	})
+
+
+
 });
